1d815612ec7834f65dbc897ba68bf449c840a055
gfcf14 <carloscuba014@gmail.com>
2024-04-30 16:05:07 -0400

    remove playground package

--- middleware/api/server.js
+++ middleware/api/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const expressPlayground = require('graphql-playground-middleware-express').default;
 const { createServer } = require('http');
 const cors = require('cors');
 const { testTypeDefs, testResolvers } = require('../src/graphql/resolvers/test/test.js');
@@ -21,16 +20,10 @@ async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
 
-  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
-
-  // Redirect from root to /playground
-  // This is purely "cosmetic" so Vercel doesn't show an error page on the dashboard
-  app.get('/', (req, res) => res.redirect('/playground'));
-
   // Only listen on HTTP port in local development, not when deployed on Vercel
   if (!process.env.VERCEL) {
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log(`🚀 Server ready at http://localhost:${PORT}/playground`));
+    app.listen(PORT, () => console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`));
   }
 }
 
