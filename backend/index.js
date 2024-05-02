a59593636636539ef52aefc1b10d14e98a6721a4
gfcf14 <carloscuba014@gmail.com>
2024-05-02 10:21:42 -0400

    refactor server to use mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,66 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+// const testRouter = require('.routes/TestRoutes');
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use("/test", testRouter);
+
+app.use('/', (req, res) => {
+  res.send('Hello World from the Node.js backend!');
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:` +
             `${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_URL}/` +
             `?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
 
-let db;
-
-async function connectToDatabase() {
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB');
-    db = client.db(process.env.DB_NAME);
-    return db;
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-    process.exit(1);
-  }
+try {
+  mongoose.connect(uri);
+} catch (error) {
+  console.log(error);
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World from the Node.js backend!');
-});
-
-app.get('/test', async (req, res) => {
-  try {
-    const collection = db.collection(process.env.DB_COLLECTION);
-    const data = await collection.find({}).toArray();
-    res.send(data);
-  } catch (error) {
-    console.error('Error accessing the database', error);
-    res.status(500).send('Failed to fetch data');
-  }
-});
-
-async function startServer() {
-  await connectToDatabase();
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-  });
-}
+});
 
-startServer();
+module.exports = app;
+
+// let db;
+// let dbReady = false;
+
+// MongoClient.connect(uri, (err, client) => {
+//     if (err) {
+//         console.error('Error connecting to MongoDB', err);
+//         return;
+//     }
+//     db = client.db(process.env.DB_NAME);
+//     console.log('Connected to MongoDB');
+//     dbReady = true;
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World from the Node.js backend!');
+// });
+
+// app.get('/test', async (req, res) => {
+//   if (!dbReady) {
+//     return res.status(503).send('Database not ready');
+//   }
+//   try {
+//     const collection = db.collection(process.env.DB_COLLECTION);
+//     const data = await collection.find({}).toArray();
+//     res.send(data);
+//   } catch (error) {
+//     console.error('Error accessing the database', error);
+//     res.status(500).send('Failed to fetch data');
+//   }
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+// });
