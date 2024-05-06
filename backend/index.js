430765c404e53e374a8c32953ec858c3905735f4
gfcf14 <carloscuba014@gmail.com>
2024-05-06 15:02:26 -0400

    test provisional backend mongodb connection code

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,27 @@
 const express = require('express');
-const connectToDb = require('./configs/db.config');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
-const testRouter = require('./routes/testRoutes');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: '*' }));
 
-app.use('/test', testRouter.getUsers);
+const log = (args) => {
+  console.log(args);
+};
 
-connectToDb()
-  .then((res) => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
-module.exports = app;
+app.get('/', (req, res) => {
+  res.send('Hello from the NodeJS backend!');
+});
+
+async function startServer() {
+  const port = process.env.PORT || 5000;
+  app.listen({ port }, () =>
+    log(`🚀 Server ready at http://localhost:${port}`)
+  );
+  require('./dbconnection');
+}
+
+startServer();
