02d88f7500427fd05218693eeec0b0abea7b57ab
gfcf14 <carloscuba014@gmail.com>
2024-05-06 15:43:54 -0400

    add back the db config, connect and fetch

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const testRouter = require('./routes/testRoutes');
+const connectToDb = require('./configs/db.config');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,12 +18,14 @@ app.get('/', (req, res) => {
   res.send('Hello from the NodeJS backend!');
 });
 
+app.use('/test', testRouter.getUsers);
+
 async function startServer() {
   const port = process.env.PORT || 5000;
   app.listen({ port }, () =>
     log(`🚀 Server ready at http://localhost:${port}`)
   );
-  require('./dbconnection');
+  connectToDb();
 }
 
 startServer();
